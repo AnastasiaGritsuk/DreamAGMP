f7c534ffdcb3b037f7d2068a26b155344d6ed42b
AnastasiaGritsuk <natikana_@mail.ru>
2018-06-17 19:26:47 +0300

    iterate throught courseList

--- src/app/course-list/course-list-item/course-list-item.component.ts
+++ src/app/course-list/course-list-item/course-list-item.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CourseListItem } from '../course-list-item';
 
 @Component({
-  selector: 'app-course-list-item',
-  templateUrl: './course-list-item.component.html',
-  styleUrls: ['./course-list-item.component.css']
+    selector: 'app-course-list-item',
+    templateUrl: './course-list-item.component.html',
+    styleUrls: ['./course-list-item.component.css']
 })
 export class CourseListItemComponent implements OnInit {
+    @Input() courseListItem: CourseListItem;
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
--- src/app/course-list/course-list/course-list.component.ts
+++ src/app/course-list/course-list/course-list.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseListItem } from '../course-list-item';
 
 @Component({
-  selector: 'app-course-list',
-  templateUrl: './course-list.component.html',
-  styleUrls: ['./course-list.component.css']
+    selector: 'app-course-list',
+    templateUrl: './course-list.component.html',
+    styleUrls: ['./course-list.component.css']
 })
 export class CourseListComponent implements OnInit {
+    public courseList: CourseListItem[] = [
+        {
+            id: 1,
+            title: "my first course"
+        },
+        {
+            id: 2,
+            title: "my second course"
+        },
+        {
+            id: 3,
+            title: "my third course"
+        }
+    ];
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
