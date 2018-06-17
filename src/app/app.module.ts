38820bacc8a5779cc20687a975f00181a63965a9
AnastasiaGritsuk <natikana_@mail.ru>
2018-06-17 16:59:45 +0300

    create corse component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { CourseListComponent } from './course-list/course-list/course-list.component';
+import { CourseListItemComponent } from './course-list/course-list-item/course-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    CourseListItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
