25c5a6b67e326e45e36c9bb1911c78adc6a87317
AnastasiaGritsuk <natikana_@mail.ru>
2018-06-17 23:40:51 +0300

    updare course-item model

--- src/app/course-list/course.service.ts
+++ src/app/course-list/course.service.ts
@@ -12,15 +12,24 @@ export class CourseService {
         return [
             {
                 id: 1,
-                title: "my first course"
+                title: "my first course",
+                creationDate: "03.31.1990",
+                duration: 60,
+                description: "desc 1"
             },
             {
                 id: 2,
-                title: "my second course"
+                title: "my second course",
+                creationDate: "03.31.1990",
+                duration: 60,
+                description: "desc 1"
             },
             {
                 id: 3,
-                title: "my third course"
+                title: "my third course",
+                creationDate: "03.31.1990",
+                duration: 60,
+                description: "desc 1"
             }
         ]
     }
