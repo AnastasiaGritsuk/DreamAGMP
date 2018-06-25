e38f88aaada75d1801a4a1986130491eeed6427f
AnastasiaGritsuk <natikana_@mail.ru>
2018-06-25 07:18:12 +0300

    add handleDelete()

--- src/app/course-list/course-list-item/course-list-item.component.ts
+++ src/app/course-list/course-list-item/course-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { CourseListItem } from '../course-list-item';
 
 @Component({
@@ -8,9 +8,14 @@ import { CourseListItem } from '../course-list-item';
 })
 export class CourseListItemComponent implements OnInit {
     @Input() public courseListItem: CourseListItem;
+    @Output() public delete: EventEmitter<number> = new EventEmitter();
     constructor() { }
 
     ngOnInit() {
+    }
+
+    public deleteItem() {
+        this.delete.emit(this.courseListItem.id);
     }
 
 }
