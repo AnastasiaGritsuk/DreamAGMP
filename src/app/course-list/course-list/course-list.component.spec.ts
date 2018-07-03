2950ad64862a5fcd88fecd381e920dad1e896e38
AnastasiaGritsuk <natikana_@mail.ru>
2018-07-03 21:35:26 +0300

    add test for courselistservice

--- src/app/course-list/course-list/course-list.component.spec.ts
+++ src/app/course-list/course-list/course-list.component.spec.ts
@@ -1,14 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CourseListComponent } from './course-list.component';
+import { CourseService } from '../course.service';
 
 describe('CourseListComponent', () => {
     let component: CourseListComponent;
     let fixture: ComponentFixture<CourseListComponent>;
+    let courseServiceMock: Partial<CourseService>;
 
     beforeEach(async(() => {
+        courseServiceMock = {
+            getCourseItems: jasmine.createSpy("getCourseItems")
+        }
         TestBed.configureTestingModule({
             declarations: [CourseListComponent],
+            providers:[
+                {
+                    provide: CourseService, 
+                    useValue: courseServiceMock}
+            ],
             schemas: [
                 CUSTOM_ELEMENTS_SCHEMA
               ],
@@ -24,5 +34,10 @@ describe('CourseListComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should call CourseListService to get items', () => {
+        component.ngOnInit();
+        expect(courseServiceMock.getCourseItems).toHaveBeenCalled();
     });
 });
