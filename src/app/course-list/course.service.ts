09a0d4bf6578753c535242800dbb2f3f18d30bb3
AnastasiaGritsuk <natikana_@mail.ru>
2018-08-21 16:44:17 +0300

    add methonds for course service

--- src/app/course-list/course.service.ts
+++ src/app/course-list/course.service.ts
@@ -5,55 +5,65 @@ import { CourseListItem } from './course-list-item';
     providedIn: 'root'
 })
 export class CourseService {
-
+    private courseList: CourseListItem[] = [
+        {
+            id: 1,
+            title: "my first course",
+            creationDate: "07/20/2018",
+            duration: 120,
+            description: "desc 1"
+        },
+        {
+            id: 2,
+            title: "my second course",
+            creationDate: "11/14/2014",
+            duration: 45,
+            description: "desc 1"
+        },
+        {
+            id: 3,
+            title: "my third course",
+            creationDate: "11/14/2019",
+            duration: 150,
+            description: "desc 1"
+        },
+        {
+            id: 4,
+            title: "my fourth course",
+            creationDate: "06/03/2011",
+            duration: 45,
+            description: "desc 4"
+        }
+    ]
     constructor() { }
 
     public getList(): CourseListItem[] {
-        return [
-            {
-                id: 1,
-                title: "my first course",
-                creationDate: "07/20/2018",
-                duration: 120,
-                description: "desc 1"
-            },
-            {
-                id: 2,
-                title: "my second course",
-                creationDate: "11/14/2014",
-                duration: 45,
-                description: "desc 1"
-            },
-            {
-                id: 3,
-                title: "my third course",
-                creationDate: "11/14/2019",
-                duration: 150,
-                description: "desc 1"
-            },
-            {
-                id: 4,
-                title: "my fourth course",
-                creationDate: "06/03/2011",
-                duration: 45,
-                description: "desc 4"
-            }
-        ]
+        return this.courseList;
     }
 
-    public createCourse(): void {
-
+    public createCourse(course: CourseListItem): void {
+        this.courseList.push(course);
     }
 
-    public getItemById(id: number): void {
-
+    public getItemById(id: number): CourseListItem {
+        return this.courseList.find((item)=> {
+            return item.id === id;
+        })
     }
 
-    public updateItem(id: number): void {
+    public updateItem(course: CourseListItem): void {
+        let index = this.courseList.findIndex((item)=> {
+            return item.id === course.id;
+        });
 
+        course[index] = course; 
     }
     
     public removeItem(id: number): void {
+        let index = this.courseList.findIndex((item)=> {
+            return item.id === id;
+        });
 
+        this.courseList.slice(index, 1);
     }
 }
