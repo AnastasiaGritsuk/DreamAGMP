622c9806c62f184960a073ee8e7e66b9083fa6b6
AnastasiaGritsuk <natikana_@mail.ru>
2018-08-28 21:55:12 +0300

    add course page

---
+++ src/app/course-page/course-page.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddCoursePageComponent } from './add-course-page/add-course-page.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AddCoursePageComponent]
+})
+export class CoursePageModule { }
