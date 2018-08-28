0c553f92e1dbd3df10998dc9d38045c1f156b3ed
AnastasiaGritsuk <natikana_@mail.ru>
2018-08-28 22:30:17 +0300

    add onPush strategy

--- src/app/course-list/course-list/course-list.component.ts
+++ src/app/course-list/course-list/course-list.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { CourseListItem } from '../course-list-item';
 
 @Component({
     selector: 'app-course-list',
     templateUrl: './course-list.component.html',
     styleUrls: ['./course-list.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    
 })
 export class CourseListComponent implements OnInit {
     @Input() courses: CourseListItem[];
