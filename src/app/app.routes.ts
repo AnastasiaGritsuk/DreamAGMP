e5902dfa0c40c552fc93b8fbdfeadf4383e3eb85
AnastasiaGritsuk <natikana_@mail.ru>
2018-09-01 22:52:46 +0300

    add 404 page to route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CourseListComponent } from './course-list/course-list/course-list.component';
 import { AddCoursePageComponent } from './course-page/add-course-page/add-course-page.component';
+import { NotFoundPageComponent } from './course-page/not-found-page/not-found-page.component';
 
 export const ROUTES: Routes = [
     { path: '', redirectTo: 'courses', pathMatch: 'full' },
@@ -13,5 +14,7 @@ export const ROUTES: Routes = [
                 component: AddCoursePageComponent
             } 
         ]
-    }
+    },
+    {path: '404', component: NotFoundPageComponent},
+    {path: '**', redirectTo: '/404'}
 ];
