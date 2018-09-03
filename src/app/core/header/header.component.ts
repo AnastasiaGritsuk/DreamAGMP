e762eecda8d2097b65998eca51f82413f5c6a64b
Anastasiya Hrytsuk <Anastasiya_Hrytsuk@epam.com>
2018-09-03 16:59:47 +0300

    add navigation on login and logout

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthorizationService } from '../../login-page/authorization.service';
-import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-header',
@@ -8,25 +7,11 @@ import { Router, ActivatedRoute } from '@angular/router';
     styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+    constructor(private authorizationService: AuthorizationService) { }
 
-    @Output() userLogout = new EventEmitter<boolean>();
-
-    constructor(
-        private authorizationService: AuthorizationService,
-        private router: Router,
-        private route: ActivatedRoute) { }
-
-    ngOnInit() {
-    }
-
-    public logout(): void {
-        this.userLogout.emit(true);
-        this.authorizationService.logout();
-        this.router.navigate(['login'], { relativeTo: this.route });
-    }
+    ngOnInit() {}
 
     public isAuth(): boolean {
         return this.authorizationService.isAuthenticated();
     }
-
 }
