4b06a66edb93c5b7753319d929aa4d3733a07b43
AnastasiaGritsuk <natikana_@mail.ru>
2018-11-21 08:30:03 +0300

    load block add

---
+++ src/app/loading-block/loading-block.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoadingBlockComponent } from './loading-block/loading-block.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [LoadingBlockComponent]
+})
+export class LoadingBlockModule { }
