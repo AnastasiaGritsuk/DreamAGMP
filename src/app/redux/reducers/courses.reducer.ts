cbdc314149bc04b68182716336b19de9ebf407ca
AnastasiaGritsuk <natikana_@mail.ru>
2018-12-14 12:47:41 +0300

    'ADD_ONE' reducer

--- src/app/redux/reducers/courses.reducer.ts
+++ src/app/redux/reducers/courses.reducer.ts
@@ -1,27 +1,34 @@
 import { CourseItem } from "src/app/components/course-list/course-list-item";
 import { Action } from "@ngrx/store";
 import { AppState } from "../app-state";
+import * as courseActions from '../actions/courses';
 
-const appState: AppState = {
-    courses: []
+const appState = {
+    ids: [],
+    courses: {} // map how to create
 }
 
-export function coursesReducer(state = appState, action: Action) {
+export function coursesReducer(state = appState, action: courseActions.Action) {
     console.log(action.type, state);
 
     switch(action.type) {
         case 'ADD_ONE':
+            const newCourse = action.payload;
             console.log('in add');
-            return state;
-        case 'UPDATE_COURSE':
-            console.log('in update');
-            return state;
-        case 'DELETE_COURSE':
-            console.log('in delete');
-            return state;
-        case 'FIND_COURSE_BY_ID':
-            console.log('in find by id');
-            return state;
+            return {
+                ...state,
+                ids: [...state.ids, newCourse.id],
+                courses: { ...state.courses, newCourse }
+            };
+        // case 'UPDATE_COURSE':
+        //     console.log('in update');
+        //     return state;
+        // case 'DELETE_COURSE':
+        //     console.log('in delete');
+        //     return state;
+        // case 'FIND_COURSE_BY_ID':
+        //     console.log('in find by id');
+        //     return state;
         default:
             return state;
     }
